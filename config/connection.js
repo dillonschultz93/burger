5baaa659e899d73b6817ba18ad984bf8f5018600
Dillon Schultz <dillonschultz93@gmail.com>
2018-02-15 23:44:09 -0700

    Setup a mysql connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  port: 3306,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'burgers_db'
+})
+
+connection.connect((err) =>{
+  if(err) {
+    console.log(`error connecting: ${err.stack}`)
+    return
+  }
+  console.log(`Connected as id: ${connection.threadId}`)
+})
+
+module.exports = connection
